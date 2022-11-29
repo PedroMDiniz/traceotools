c9b7605a0e26ae7800e3d088e799ba3731425a7a
Pedro Diniz <119310054+PedroMDiniz@users.noreply.github.com>
2022-11-29 15:42:55 -0300

    Update testfile.py

--- testfile.py
+++ testfile.py
@@ -18,7 +18,7 @@ from traceotools import *
 import os
 
 ### CHANGE TO CORRECT PATH ###
-traceopath = 'C:/Users/mdped/Desktop/AT/bin/'
+traceopath = 'C:\\Users\\mdped\\Desktop\\AT\\bin\\'
 
 case_title = 'Test file'
 fname = 'test'
